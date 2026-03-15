741b8da8abbb8cf6448b1ae8091230253e333024
Thepaganec <thepaganec@gmail.com>
2026-03-15 16:20:15 +0300

    не писать __pycache__

--- run_agent.py
+++ run_agent.py
@@ -1,4 +1,10 @@
 import asyncio
+import os
+import sys
+
+sys.dont_write_bytecode = True
+os.environ["PYTHONDONTWRITEBYTECODE"] = "1"
+
 from core.agent.agent_server import main
 
 if __name__ == "__main__":
--- run_scheduler_worker.py
+++ run_scheduler_worker.py
@@ -1,4 +1,9 @@
 import asyncio
+import os
+import sys
+
+sys.dont_write_bytecode = True
+os.environ["PYTHONDONTWRITEBYTECODE"] = "1"
 
 from core.scheduler_service.worker import run_worker
 
